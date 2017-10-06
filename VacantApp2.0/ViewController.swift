bc5c0c01c3a85fc40ca734ee1f54feca9608273d
Kyle Nakamura <knakamura13@apu.edu>
2017-10-05 22:22:47 -0700

    Initial Commit

---
+++ VacantApp2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VacantApp2.0
+//
+//  Created by Kyle Nakamura on 10/5/17.
+//  Copyright © 2017 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
